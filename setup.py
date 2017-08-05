2d7b15cdf56f4338d8945611db1f76be93372987
Adrian Albert <adalbert@ocio-gpu01.slac.stanford.edu>
2017-08-05 05:49:55 -0700

    published v0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pysatml',
-      version='0.1.2',
+      version='0.1.0',
       description='Satellite data imagery preprocessing and formatting functionality for typical machine learning/computer vision tasks.',
       url='https://github.com/adrianalbert/pysatml',
       author='Adrian Albert',
